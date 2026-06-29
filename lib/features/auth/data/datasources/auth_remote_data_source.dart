3edd58ef682e3475a1f70e26f0e5f45abe7c3195
al-amin-bhuiyan <alexal018827@gmail.com>
2026-06-29 19:25:00 +0600

    feat(auth): define remote and local auth data sources

---
+++ lib/features/auth/data/datasources/auth_remote_data_source.dart
@@ -0,0 +1,17 @@
+import '../models/user_model.dart';
+import '../models/user_profile_model.dart';
+
+abstract class AuthRemoteDataSource {
+  Future<UserModel> login({
+    required String email,
+    required String password,
+  });
+
+  Future<UserModel> register({
+    required String name,
+    required String email,
+    required String password,
+  });
+
+  Future<UserProfileModel> getUserProfile();
+}

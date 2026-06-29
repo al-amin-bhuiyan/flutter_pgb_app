8d9bafbbf58300b2457c061a923a0f3522988d43
al-amin-bhuiyan <alexal018827@gmail.com>
2026-06-29 16:37:00 +0600

    feat(todos): define todo repository interface

---
+++ lib/features/todos/domain/repositories/todos_repository.dart
@@ -0,0 +1,9 @@
+import '../entities/todo.dart';
+
+abstract class TodosRepository {
+  Future<List<Todo>> getTodos();
+
+  Future<void> saveTodo(Todo todo);
+
+  Future<void> syncTodos(List<Todo> todos);
+}

e83ea0bdf3583afe58209091307b2704812fe002
al-amin-bhuiyan <alexal018827@gmail.com>
2026-06-29 20:35:00 +0600

    feat(locations): implement remote geofences api calls

---
+++ lib/features/locations/data/datasources/locations_remote_data_source_impl.dart
@@ -0,0 +1,84 @@
+import 'package:dio/dio.dart';
+import '../../../../core/error/exceptions.dart';
+import '../../../../core/network/dio_client.dart';
+import '../models/geofence_location_model.dart';
+import 'locations_remote_data_source.dart';
+
+class LocationsRemoteDataSourceImpl implements LocationsRemoteDataSource {
+  final DioClient _client;
+
+  LocationsRemoteDataSourceImpl({
+    required DioClient client,
+  }) : _client = client;
+
+  @override
+  Future<List<GeofenceLocationModel>> getLocations() async {
+    try {
+      final response = await _client.dio.get('/locations');
+
+      if (response.statusCode == 200) {
+        final list = response.data as List<dynamic>;
+        return list
+            .map((item) => GeofenceLocationModel.fromJson(item as Map<String, dynamic>))
+            .toList();
+      } else {
+        throw ServerException(
+          message: response.statusMessage,
+          statusCode: response.statusCode,
+        );
+      }
+    } on DioException catch (e) {
+      throw ServerException(
+        message: e.response?.data?['message'] as String? ?? e.message,
+        statusCode: e.response?.statusCode,
+      );
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+
+  @override
+  Future<void> saveLocation(GeofenceLocationModel location) async {
+    try {
+      final response = await _client.dio.post(
+        '/locations',
+        data: location.toJson(),
+      );
+
+      if (response.statusCode != 200 && response.statusCode != 201) {
+        throw ServerException(
+          message: response.statusMessage,
+          statusCode: response.statusCode,
+        );
+      }
+    } on DioException catch (e) {
+      throw ServerException(
+        message: e.response?.data?['message'] as String? ?? e.message,
+        statusCode: e.response?.statusCode,
+      );
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+
+  @override
+  Future<void> deleteLocation(String id) async {
+    try {
+      final response = await _client.dio.delete('/locations/$id');
+
+      if (response.statusCode != 200 && response.statusCode != 204) {
+        throw ServerException(
+          message: response.statusMessage,
+          statusCode: response.statusCode,
+        );
+      }
+    } on DioException catch (e) {
+      throw ServerException(
+        message: e.response?.data?['message'] as String? ?? e.message,
+        statusCode: e.response?.statusCode,
+      );
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+}
--- lib/features/locations/data/models/geofence_location_model.dart
+++ lib/features/locations/data/models/geofence_location_model.dart
@@ -38,6 +38,26 @@ class GeofenceLocationModel {
     );
   }
 
+  factory GeofenceLocationModel.fromJson(Map<String, dynamic> json) {
+    return GeofenceLocationModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      latitude: (json['latitude'] as num).toDouble(),
+      longitude: (json['longitude'] as num).toDouble(),
+      radius: (json['radius'] as num).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'latitude': latitude,
+      'longitude': longitude,
+      'radius': radius,
+    };
+  }
+
   GeofenceLocation toEntity() {
     return GeofenceLocation(
       id: id,
